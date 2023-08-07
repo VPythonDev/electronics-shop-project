ff926847dbbab4f62f3e9a4a9af8caa32f17073a
Valera Babkin <valera.babkin.13@mail.ru>
2023-08-07 17:45:22 +0300

    Implemented class Item with tests

--- src/item.py
+++ src/item.py
@@ -13,7 +13,14 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        # Добаляет объект в список класса - all
+        self.all.append(self)
+
+    def __repr__(self):
+        return f"{self.name}, цена: {self.price}, кол-во: {self.quantity}"
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +28,11 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
+        return self.price
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,21 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+
+@pytest.fixture
+def smartphone():
+    return Item("Смартфон", 15000, 10)
+
+
+def test_calculate_total_price(smartphone):
+    assert smartphone.calculate_total_price() == 150000
+
+
+def test_apply_discount(smartphone):
+    Item.pay_rate = 0.5
+    assert smartphone.apply_discount() == 7500
+
+
+def test_repr(smartphone):
+    assert repr(smartphone) == "Смартфон, цена: 15000, кол-во: 10"
